b936fd374478c7975b370f9a3dd53a4f82262f5d
Logan McAnsh <logan@mcan.sh>
2017-05-22 11:36:50 -0400

    update raven version #

--- public/javascripts/modules/raven.js
+++ public/javascripts/modules/raven.js
@@ -4,7 +4,7 @@ function setupRaven() {
   if (location.origin !== 'localhost:7777') {
     Raven
       .config('https://2b010463aa1242ca8ecda627539c5338@sentry.io/170090', {
-        release: '0.0.2'
+        release: '0.0.3-1'
       })
       .install();
   }
