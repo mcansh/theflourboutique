e483d95d14b1ce79d505bb87c6bf793ff37d9079
Logan McAnsh <logan@mcan.sh>
2017-07-31 00:39:13 -0400

    fix comments

--- start.js
+++ start.js
@@ -24,6 +24,7 @@ require('./models/Order');
 
 // Start our app!
 const app = require('./app');
+
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
