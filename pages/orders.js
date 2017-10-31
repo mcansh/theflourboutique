c80438b8584d2e8ceee8f2790b4be72e07eabe24
Logan McAnsh <logan@mcan.sh>
2017-10-31 15:44:43 -0400

    rename orders page from `Order` to `Orders`

--- pages/orders.js
+++ pages/orders.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import withData from '../lib/withData';
 import Page from '../components/Page';
 import AllOrders from '../components/AllOrders';
 
-const Order = withData(() => (
-  <Page>
+const Orders = withData(({ url }) => (
+  <Page pathname={url.pathname}>
     <AllOrders />
   </Page>
 ));
 
-export default Order;
+Orders.propTypes = {
+  url: PropTypes.shape({
+    pathname: PropTypes.string,
+  }).isRequired,
+};
+
+export default Orders;
