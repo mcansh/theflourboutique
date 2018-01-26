92b5a135c681969b9b30deb9ad0e81a9cd3dc2a1
Logan McAnsh <logan@mcan.sh>
2018-01-26 16:14:04 -0500

    switch server to polka

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { createServer } from 'http';
+import polka from 'polka';
 import { parse } from 'url';
 import next from 'next';
 import { join } from 'path';
@@ -11,21 +11,23 @@ const PORT = process.env.PORT || 3000;
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
+  const server = polka();
+
+  server.get('/sw.js', (req, res) => {
     const parsedUrl = parse(req.url, true);
     const { pathname } = parsedUrl;
-
-    if (/^\/sw.js\/?$/.test(pathname)) {
-      const filePath = join(__dirname, '.next', pathname);
-      app.serveStatic(req, res, filePath);
-    } else if (/^\/manifest(.json)?\/?$/.test(pathname)) {
-      res.setHeader('Content-Type', 'application/json');
-      res.end(manifest());
-    } else {
-      handle(req, res, parsedUrl);
-    }
-  }).listen(PORT, err => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${PORT}`); // eslint-disable-line no-console
+    const filePath = join(__dirname, '.next', pathname);
+    app.serveStatic(req, res, filePath);
   });
+
+  server.get('/manifest.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(manifest());
+  });
+
+  server.get('*', (req, res) => handle(req, res));
+
+  server
+    .listen(PORT)
+    .then(() => console.log(`> Ready on http://localhost:${PORT}`)); // eslint-disable-line no-console
 });
