4c38213f6d99c3ae045ee87234107f69fe379804
Logan McAnsh <logan@mcan.sh>
2018-02-10 18:03:32 -0500

    this shop page is 10/10

---
+++ pages/shop.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import withData from '../lib/withData';
+import Page from '../components/Page';
+
+const Shop = ({ data: { loading, error, allProducts } }) => {
+  if (error) {
+    return (
+      <p>
+        An error has occured and we have been notified, sorry about that{' '}
+        <span role="img" aria-label="sad face emoji">
+          ☹️
+        </span>
+      </p>
+    );
+  }
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return <Page>{allProducts.map(product => <p>{product.name}</p>)}</Page>;
+};
+
+Shop.propTypes = {
+  data: PropTypes.shape({
+    loading: PropTypes.bool,
+    error: PropTypes.oneOfType([
+      PropTypes.bool,
+      PropTypes.array,
+      PropTypes.shape({}),
+    ]),
+    allProducts: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string,
+        price: PropTypes.number,
+        description: PropTypes.string,
+        image: PropTypes.string,
+      })
+    ),
+  }).isRequired,
+};
+
+const AllProductsQuery = gql`
+  query allProducts {
+    allProducts {
+      name
+      price
+      description
+      image
+    }
+  }
+`;
+
+const GraphQLAllProducts = graphql(AllProductsQuery, {
+  props: ({ data }) => ({
+    data,
+  }),
+})(Shop);
+
+export default withData(GraphQLAllProducts);
