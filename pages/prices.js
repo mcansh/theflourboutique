26af1bceb8c440ada742d0acb6af690788c68dce
Logan McAnsh <logan@mcan.sh>
2018-02-10 19:09:40 -0500

    update price copy

--- pages/prices.js
+++ pages/prices.js
@@ -27,12 +27,12 @@ const Prices = ({ data: { loading, error, allPrices } }) => {
     <Page title="Price Guide">
       <Huge text="The Flour Boutique Price Guide" />
       <PriceChart prices={allPrices} />
-      <Paragraph>
+      <Paragraph size={2}>
         Please note: these are approximate sizes, they may not be the full size
         of the box, price goes by whichever side is the longest. There is some
         exception to the price: if a cookie 5”x3” it would go by the 4” size.
         The majority of cookies fall into the detail range and most are at least
-        3-4” in size. All cookies are sold in 1 dozen or unless I am offering a
+        3-4” in size. All cookies are sold in 1 dozen unless I am offering a
         prepackaged set. If ordering a set of multiple cookies (different
         sizes), the cost of a dozen is $40.
       </Paragraph>
