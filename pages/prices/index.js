1d7e4aad8cc3a9e2b6db26f2538b2e23e9616716
Logan McAnsh <logan@mcan.sh>
2018-02-27 23:42:55 -0500

    update price page to use apollo-boost
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- pages/prices/index.js
+++ pages/prices/index.js
@@ -1,44 +1,10 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
+import React, { Fragment } from 'react';
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
 import withData from '../../lib/withData';
 import Page from '../../components/Page';
 import { Huge, Paragraph } from '../../components/Type';
 import PriceChart from '../../components/PriceChart';
-
-const Prices = ({ data: { loading, error, allPrices } }) => {
-  if (error) {
-    return (
-      <p>
-        An error has occured and we have been notified, sorry about that{' '}
-        <span role="img" aria-label="sad face emoji">
-          ☹️
-        </span>
-      </p>
-    );
-  }
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  return (
-    <Page title="Price Guide">
-      <Huge text="The Flour Boutique Price Guide" />
-      <PriceChart prices={allPrices} />
-      <Paragraph size={2}>
-        Please note: these are approximate sizes, they may not be the full size
-        of the box, price goes by whichever side is the longest. There is some
-        exception to the price: if a cookie 5”x3” it would go by the 4” size.
-        The majority of cookies fall into the detail range and most are at least
-        3-4” in size. All cookies are sold in 1 dozen unless I am offering a
-        prepackaged set. If ordering a set of multiple cookies (different
-        sizes), the cost of a dozen is $40.
-      </Paragraph>
-    </Page>
-  );
-};
 
 const PricesQuery = gql`
   query allPrices {
@@ -51,29 +17,30 @@ const PricesQuery = gql`
   }
 `;
 
-const GraphQLPrices = graphql(PricesQuery, {
-  props: ({ data }) => ({
-    data,
-  }),
-})(Prices);
+const Prices = () => (
+  <Page title="Price Guide">
+    <Query query={PricesQuery}>
+      {({ loading, error, data: { allPrices } }) => {
+        if (loading) return <Huge text="Loading..." />;
+        if (error) return <Huge text="Error :(" />;
+        return (
+          <Fragment>
+            <Huge text="The Flour Boutique Price Guide" />
+            <PriceChart prices={allPrices} />
+            <Paragraph size={2}>
+              Please note: these are approximate sizes, they may not be the full
+              size of the box, price goes by whichever side is the longest.
+              There is some exception to the price: if a cookie 5”x3” it would
+              go by the 4” size. The majority of cookies fall into the detail
+              range and most are at least 3-4” in size. All cookies are sold in
+              1 dozen unless I am offering a prepackaged set. If ordering a set
+              of multiple cookies (different sizes), the cost of a dozen is $40.
+            </Paragraph>
+          </Fragment>
+        );
+      }}
+    </Query>
+  </Page>
+);
 
-Prices.propTypes = {
-  data: PropTypes.shape({
-    loading: PropTypes.bool,
-    error: PropTypes.oneOfType([
-      PropTypes.bool,
-      PropTypes.array,
-      PropTypes.shape({}),
-    ]),
-    allPrices: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string,
-        size: PropTypes.number,
-        basicPrice: PropTypes.number,
-        detailedPrice: PropTypes.number,
-      })
-    ),
-  }).isRequired,
-};
-
-export default withData(GraphQLPrices);
+export default withData(Prices);
