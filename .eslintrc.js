12dc17f37decf99ad424540ccf5f7544a61e5e29
Logan McAnsh <logan@mcan.sh>
2018-03-09 14:00:10 -0500

    update eslintrc
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['mcansh'],
   rules: {
-    'no-unused-expressions': ["error", { "allowTaggedTemplates": true }]
+    'no-unused-expressions': [2, { "allowTaggedTemplates": true }],
+    'jsx-a11y/anchor-is-valid': 0,
   }
 };
