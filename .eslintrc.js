f25f6abd11313ae109b5f84b061766f90b863f75
Logan McAnsh <logan@mcan.sh>
2018-03-09 13:00:47 -0500

    move global styles to injectGlobal

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: ['mcansh']
+  extends: ['mcansh'],
+  rules: {
+    'no-unused-expressions': ["error", { "allowTaggedTemplates": true }]
+  }
 };
--- components/Page.js
+++ components/Page.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import Head from 'next/head';
 import NProgress from 'nprogress';
+import { injectGlobal } from 'styled-components';
 import Router, { withRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import withIntl from '../lib/withIntl';
@@ -9,8 +10,68 @@ import Back2Home from '../components/Back2Home';
 import { initGA, logPageView } from '../lib/analytics';
 import withSentry from '../lib/withSentry';
 import Wrapper from './Wrapper';
-
 import { colors } from '../theme';
+
+injectGlobal`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    font-weight: 300;
+    margin: 0;
+  }
+
+  html {
+    font-size: 10px;
+  }
+
+  body {
+    font-family: 'Open Sans';
+    line-height: 1.15;
+    text-size-adjust: 100%;
+    font-weight: 300;
+    padding-top: env(safe-area-inset-top);
+    padding-left: env(safe-area-inset-left);
+    padding-right: env(safe-area-inset-right);
+    padding-bottom: env(safe-area-inset-bottom);
+    padding-top: constant(safe-area-inset-top);
+    padding-left: constant(safe-area-inset-left);
+    padding-right: constant(safe-area-inset-right);
+    padding-bottom: constant(safe-area-inset-bottom);
+    min-height: 100vh;
+  }
+
+  #nprogress .bar {
+    background: ${colors.pink};
+    position: fixed;
+    z-index: 1031;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 0.2rem;
+  }
+
+  #nprogress .peg {
+    display: block;
+    position: absolute;
+    right: 0;
+    width: 10rem;
+    height: 100%;
+    box-shadow: 0 0 1rem ${colors.pink}, 0 0 0.5rem ${colors.pink};
+    opacity: 1;
+    transform: rotate(3deg) translate(0, -0.4rem);
+  }
+
+  .nprogress-custom-parent {
+    overflow: hidden;
+    position: relative;
+  }
+
+  .nprogress-custom-parent #nprogress .spinner,
+  .nprogress-custom-parent #nprogress .bar {
+    position: absolute;
+  }
+`;
 
 NProgress.configure({ showSpinner: false });
 Router.onRouteChangeStart = () => NProgress.start();
@@ -37,6 +98,7 @@ class Page extends React.Component {
   }
   render() {
     const { title, children, router } = this.props;
+    const notHome = router && router.pathname && router.pathname !== '/';
     return (
       <Fragment>
         <Head>
@@ -48,69 +110,9 @@ class Page extends React.Component {
             rel="stylesheet"
           />
         </Head>
-        {router && router.pathname && router.pathname !== '/' && <Back2Home />}
+        {notHome && <Back2Home />}
         <Wrapper>{children}</Wrapper>
         <Footer />
-        <style jsx global>{`
-          *,
-          *::before,
-          *::after {
-            box-sizing: border-box;
-            font-weight: 300;
-            margin: 0;
-          }
-
-          html {
-            font-size: 10px;
-          }
-
-          body {
-            font-family: 'Open Sans';
-            line-height: 1.15;
-            text-size-adjust: 100%;
-            font-weight: 300;
-            padding-top: env(safe-area-inset-top);
-            padding-left: env(safe-area-inset-left);
-            padding-right: env(safe-area-inset-right);
-            padding-bottom: env(safe-area-inset-bottom);
-            padding-top: constant(safe-area-inset-top);
-            padding-left: constant(safe-area-inset-left);
-            padding-right: constant(safe-area-inset-right);
-            padding-bottom: constant(safe-area-inset-bottom);
-            min-height: 100vh;
-          }
-
-          #nprogress .bar {
-            background: ${colors.pink};
-            position: fixed;
-            z-index: 1031;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 0.2rem;
-          }
-
-          #nprogress .peg {
-            display: block;
-            position: absolute;
-            right: 0;
-            width: 10rem;
-            height: 100%;
-            box-shadow: 0 0 1rem ${colors.pink}, 0 0 0.5rem ${colors.pink};
-            opacity: 1;
-            transform: rotate(3deg) translate(0, -0.4rem);
-          }
-
-          .nprogress-custom-parent {
-            overflow: hidden;
-            position: relative;
-          }
-
-          .nprogress-custom-parent #nprogress .spinner,
-          .nprogress-custom-parent #nprogress .bar {
-            position: absolute;
-          }
-        `}</style>
       </Fragment>
     );
   }
